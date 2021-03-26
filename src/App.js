852411eecc8e781c72cc49fab1b50d56bea1e9bc
Karolis Kimtys <karolis_k_7@hotmail.com>
2021-03-26 21:34:01 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
-function App() {
+const App = () => {
+  const [isVisible, setVisible] = useState(false);
+  const toggleVisible = () => {
+    setVisible(isVisible === false ? true : false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onClick={() => toggleVisible()} className='App'>
+      <div className={"State"}>
+        <h3>Click anywhere inside viewport.</h3>
+        <h5>This is Framer Motion Entry and Exit animation test code.</h5>
+        <div>
+          Component has
+          {isVisible ? (
+            <h5 style={{ color: "green" }}> mounted</h5>
+          ) : (
+            <h5 style={{ color: "red" }}>unmounted</h5>
+          )}
+        </div>
+      </div>
+      <AnimatePresence>
+        {isVisible && (
+          <motion.div
+            key='1'
+            initial={{
+              scale: 0,
+              x: -1000,
+              opacity: 0,
+              transition: { duration: 1 },
+            }}
+            animate={{
+              scale: 1,
+              x: 0,
+              opacity: 1,
+              transition: { duration: 1 },
+            }}
+            exit={{
+              scale: 0,
+              x: 1000,
+              opacity: 0,
+              transition: { duration: 1 },
+            }}
+          >
+            <img src={logo} className='App-logo' alt='logo' />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
-}
+};
 
 export default App;
